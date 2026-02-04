59019fc715d3f543466c13f9496c5c0fdd73466c
RJ <riley.bell6@education.nsw.gov.au>
2026-02-05 09:25:52 +1100

    started code
    i cannot get flask to work at all im going insane

--- Code.py
+++ Code.py
@@ -1,1 +1,15 @@
+from Fask import Flask, render_templete, request, redirect, url_for, session, flash 
+from flask_sqlalchemy import SQLAlchemy
+from workzeug.security import generate_password_hash, check_password_hash
+from dataetime import datetime  
 
+#app configurations from sir
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey" #will be changed this is just code from sir to start of with
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tastetracker_ps.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app) 
+
+#data base modules also from sir
